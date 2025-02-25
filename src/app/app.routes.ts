2b9934f3683a9d728c56be7193988e4cb447ebe9
Nhi.Nguyen <ngnhi.nguyen@gmail.com>
2025-02-25 16:53:10 +0100

    Initialer Commit: Projekt hochgeladen

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+import { CreateComponent } from './create/create.component';
+import { DetailComponent } from './detail/detail.component';
+
+export const routes: Routes = [{
+    path: "",
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: "table",
+    component: TableComponent
+  },
+  {
+    path: "create",
+    component: CreateComponent
+  },
+  {
+    path: "member/:id",
+    component: DetailComponent
+  }];
